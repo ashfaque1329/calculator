0ff1953bc2b43ab479c97c66385786d1b27f53a2
Ashfaque Chowdhury <asfaque1329@gmail.com>
2023-02-26 10:25:44 +0530

    first commit

---
+++ components/Horizontal.js
@@ -0,0 +1,14 @@
+import { StyleSheet, View } from "react-native";
+
+const Horizontal = ({ children }) => {
+  return <View style={styles.container}>{children}</View>;
+};
+
+// create styles of Row
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+});
+
+export default Horizontal;
